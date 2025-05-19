7c78cad06f58f3a5add22d949d8167bda3c68e95
hanyahunya <java020103@gmail.com>
2025-05-19 23:47:30 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginBtn').addEventListener('click', () => {
+    alert('로그인 페이지로 이동합니다.');
+  });
+  
+  document.getElementById('signupBtn').addEventListener('click', () => {
+    alert('회원가입 페이지로 이동합니다.');
+  });
+  
