be15a8246ec413425af4aaeb6bd19d9d38e06da4
hanyahunya <java020103@gmail.com>
2025-05-22 21:34:16 +0900

    test

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ if (userLang.startsWith('en')) {
 }
 
 
-// document.getElementById('loginBtn').addEventListener('click', () => {
-//     alert('로그인 페이지로 이동합니다.');
-//   });
+document.getElementById('loginBtn').addEventListener('click', () => {
+    window.location.href = 'login.html';
+    // alert('로그인 페이지로 이동합니다.');
+  });
   
 //   document.getElementById('signupBtn').addEventListener('click', () => {
 //     alert('회원가입 페이지로 이동합니다.');
