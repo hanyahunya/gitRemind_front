bcff221877ef437136ff977645d368208c475e4c
hanyahunya <java020103@gmail.com>
2025-05-27 23:26:52 +0900

    delete import

--- signup.js
+++ signup.js
@@ -1,4 +1,4 @@
-import{ API_BASE_URL } from './config.js';
+const API_BASE_URL = "http://localhost:8080";
 
 const lang = navigator.language.startsWith('ja') ? 'ja'
            : navigator.language.startsWith('en') ? 'en'
